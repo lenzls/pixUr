1efbf645012d56f924ce035006236c7cafac9e6b
Simon Lenz <post@simon-lenz.de>
2020-04-24 17:53:28 +0200

    long args for readablity in scripts

--- build/deploy-github-pages.sh
+++ build/deploy-github-pages.sh
@@ -5,8 +5,8 @@ git stash
 git branch --delete --force gh-pages
 git checkout --orphan gh-pages
 git add --force dist
-git commit -m "Rebuild GitHub pages"
-git filter-branch -f --prune-empty --subdirectory-filter dist
+git commit --message "Rebuild GitHub pages"
+git filter-branch --force --prune-empty --subdirectory-filter dist
 git push --force origin gh-pages
 git co master
 echo "Deploy finished. Please visit https://lenzls.github.io/pixUr/"
