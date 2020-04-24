4dac8cd5549341761c61fc0ad7f93459850f518e
Simon Lenz <post@simon-lenz.de>
2020-04-24 16:22:45 +0200

    add html webpack plugin

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,10 +1,12 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, '..', 'dist'),
     },
     mode: process.env.NODE_ENV === 'development' ? 'development' : 'production',
+    plugins: [new HtmlWebpackPlugin()],
 };
