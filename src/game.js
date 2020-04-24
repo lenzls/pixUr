cea874c82834062a7685ab23f1ae0feafb661c20
Simon Lenz <post@simon-lenz.de>
2020-04-24 18:18:43 +0200

    add initial pixi test

--- src/game.js
+++ src/game.js
@@ -4,3 +4,45 @@ import white from './assets/graphics/white.png';
 import * as PIXI from 'pixi.js';
 
 console.log(`WebGL is supported by your browser: ${PIXI.utils.isWebGLSupported()}`);
+
+const app = new PIXI.Application({ width: 256, height: 256 });
+document.body.appendChild(app.view);
+
+const loader = PIXI.Loader.shared;
+const Sprite = PIXI.Sprite;
+const Container = PIXI.Container;
+
+const whitePieces = [];
+const blackPieces = [];
+
+const menuScene = new Container();
+const gameScene = new Container();
+
+const state = play;
+
+function play(delta) {
+    whitePieces.forEach(sprite => sprite.x += 1 * delta);
+}
+
+loader
+    .add(black)
+    .add(white)
+    .on('progress', (loader, resource) => {
+        console.log(`loading ${resource.url}`);
+        console.log(`progress: ${loader.progress}%`);
+    })
+    .load(() => {
+        menuScene.visible = false;
+        gameScene.visible = false;
+
+        whitePieces.push(new Sprite(loader.resources[white].texture));
+        blackPieces.push(new Sprite(loader.resources[black].texture));
+        
+        whitePieces.forEach(sprite => gameScene.addChild(sprite));
+        blackPieces.forEach(sprite => gameScene.addChild(sprite));
+
+        app.stage.addChild(menuScene);
+        app.stage.addChild(gameScene);
+
+        app.ticker.add(state);
+    });
