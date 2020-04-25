86e1e79ff211dd8809ec5729683617c7a4210256
Simon Lenz <post@simon-lenz.de>
2020-04-25 21:50:17 +0200

    add fullscreen button

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import { addAssets } from './sprites.js';
 import { utils, Application, loader } from './engine.js';
 import { CreateStateMachine } from './state-machine.js';
-
+import { createButton } from './scenes/common.js';
 
 console.log(`WebGL is supported by your browser: ${utils.isWebGLSupported()}`);
 
+const body = document.querySelector('body');
 const app = new Application({ 
     width: 640,
     height: 480,
@@ -18,5 +19,11 @@ addAssets({ loader })
         console.log(`progress: ${loader.progress}%`);
     })
     .load(() => {
+        app.stage.addChild(createButton({
+            text: 'Fullscreen',
+            color: 0xFF00FF,
+            position: { x: 450, y: 30 },
+            onClick: () => body.requestFullscreen(),
+        }));
         CreateStateMachine({ app });
     });
