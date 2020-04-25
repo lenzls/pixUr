48d5867601b0c171e257a3d61a626c1e391178e9
Simon Lenz <post@simon-lenz.de>
2020-04-25 02:36:56 +0200

    implement whole movement

--- src/board.js
+++ src/board.js
@@ -31,12 +31,46 @@ export function CreateBoard() {
             }
         },
         updatePieceRenderingPositions() {
-            const calcYPos = (piece) => piece.player.type === TYPE.WHITE ? 50 : 340;
-            spaces.forEach((space, spaceIndex) => {
-                space.forEach(piece => {
-                    const localIndex = space.filter(p => p.player === piece.player).indexOf(piece);
-                    piece.sprite.position.set(10 + 40 * spaceIndex, calcYPos(piece) + localIndex * 10);
-                });
+            const calcYPos = ({ player, index }) => {
+                if (index > 4 && index < 13) return 120;
+                return player.type === TYPE.WHITE ? 50 : 340;
+            };
+
+            spaces.forEach((space, index) => {
+                if (index === 0) {
+                    space.forEach(piece => {
+                        const localIndex = spaces[0].filter(p => p.player === piece.player).indexOf(piece);
+                        piece.sprite.position.set(10, calcYPos({ ...piece, index }) + localIndex * 10);
+                    });
+                }
+                else if (index <= 4) {
+                    const piece = spaces[index][0];
+                    if (piece) { 
+                        const x = 10 + (4 - index) * 36;
+                        piece.sprite.position.set(x, calcYPos({ ...piece, index })); 
+                    }
+                }
+                else if (index <= 12) {
+                    const piece = spaces[index][0];
+                    if (piece) { 
+                        const x = 10 + (index - 5) * 36;
+                        piece.sprite.position.set(x, calcYPos({ ...piece, index })); 
+                    }
+                }
+                else if (index <= 14) {
+                    const piece = spaces[index][0];
+                    if (piece) { 
+                        const x = 500 + (14 - index) * 36;
+                        piece.sprite.position.set(x, calcYPos({ ...piece, index })); 
+                    }
+                }
+                else if (index === 15) {
+                    space.forEach(piece => {
+                        const localIndex = spaces[15].filter(p => p.player === piece.player).indexOf(piece);
+                        piece.sprite.position.set(600, calcYPos({ ...piece, index }) + localIndex * 10);
+                    });
+                }
+
             });
         },
     };
