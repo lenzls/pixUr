be6078adf8dfd3fe26fa45d7690786a3b7bc151d
Simon Lenz <post@simon-lenz.de>
2020-04-26 12:50:26 +0200

    extract helper methods

--- src/game.js
+++ src/game.js
@@ -32,6 +32,7 @@ export function CreateGame() {
             }
         },
         moveAttempt({ piece }) {
+            console.log('attempting to move', piece);
             if (piece.player !== this.currentPlayer) {
                 alert("It's not your turn, pal");
                 return;
@@ -40,9 +41,7 @@ export function CreateGame() {
                 alert('Roll the dice first, pal');
                 return;
             }
-            console.log('attempting to move', piece);
             const index = board.getIndex({ piece });
-            console.log('current index', index);
             const aim = index + pips(this.lastRoll);
             
             const movePiece = ({ piece, start, aim }) => {
@@ -55,21 +54,28 @@ export function CreateGame() {
                 board.removePiece({ piece: opponentPiece, index });
                 board.addPiece({ piece: opponentPiece, index: 0 });
             };
-            if (aim <= 4 || (aim > 12 && aim <= 14)) {
-                if (board.getPieces({ index: aim }).filter(p => p.player === this.currentPlayer).length === 0) {
+            const noOwnPiecesInSpace = ({ player, index }) => board.getPieces({ index }).every(p => p.player !== player);
+            const noPiecesInSpace = ({ index }) => board.getPieces({ index }).length > 0;
+            const inSafeZone = (index) => (0 <= index && index <= 4) || (13 <= index && index <= 14);
+            const inCombatZone = (index) => (5 <= index && index <= 12);
+            const inGoal = (index) => index === 15;
+            const behindGoal = (index) => index >= 16;
+
+            if (inSafeZone(aim)) {
+                if (noOwnPiecesInSpace({ player: this.currentPlayer, index: aim })) {
                     movePiece({ piece, start: index, aim });
                 }
             }
-            else if (aim >= 4 && aim <= 12) {
-                if (board.getPieces({ index: aim }).length > 0) {
+            else if (inCombatZone(aim)) {
+                if (noPiecesInSpace({ index: aim })) {
                     combat({ piece, index: aim });
                 }
                 movePiece({ piece, start: index, aim });
             }
-            else if (aim === 15) {
+            else if (inGoal(aim)) {
                 movePiece({ piece, start: index, aim });
             }
-            else {
+            else if (behindGoal(aim)) {
                 console.log('Too far, pal.');
             }
         },
