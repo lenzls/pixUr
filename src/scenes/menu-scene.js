065a372f80a19489ad0bf277b82f5912bd0dbe6e
Simon Lenz <post@simon-lenz.de>
2020-04-28 23:15:52 +0200

    add new styled continue button

--- src/scenes/menu-scene.js
+++ src/scenes/menu-scene.js
@@ -1,25 +1,25 @@
 import { Container } from '../engine.js';
 import { STATES } from '../state-machine.js';
-import { createButton } from './common.js';
 import { ASSETS, CreateSprite } from '../sprites.js';
 
 export function createMenuScene({ stateMachine }) {
     const container = new Container();
 
     container.addChild(CreateSprite({ asset: ASSETS.PLATE }));
+
     const newGameButtonSprite = CreateSprite({ asset: ASSETS.NEW_GAME_BUTTON });
     newGameButtonSprite.interactive = true;
     newGameButtonSprite.buttonMode = true;
     newGameButtonSprite.on('pointerdown', () => stateMachine.startNewState({ state: STATES.GAME }));
-    newGameButtonSprite.position.set(60, 355);
+    newGameButtonSprite.position.set(74, 355);
     container.addChild(newGameButtonSprite);
+    
+    const continueButtonSprite = CreateSprite({ asset: ASSETS.CONTINUE_BUTTON });
+    continueButtonSprite.interactive = true;
+    continueButtonSprite.buttonMode = true;
+    continueButtonSprite.on('pointerdown', () => stateMachine.switchToState({ state: STATES.GAME }));
+    continueButtonSprite.position.set(420, 355);
+    container.addChild(continueButtonSprite);
 
-
-    container.addChild(createButton({
-        text: 'Continue',
-        color: 0x66CCFF,
-        position: { x: 420, y: 355 },
-        onClick: () => stateMachine.switchToState({ state: STATES.GAME }),
-    }));
     return container;
 }
