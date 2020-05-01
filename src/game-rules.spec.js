8f0dfed77fb96f7821ae77809d1cf270d418832e
Simon Lenz <post@simon-lenz.de>
2020-05-01 12:28:30 +0200

    occupied by own piece test

--- src/game-rules.spec.js
+++ src/game-rules.spec.js
@@ -5,15 +5,34 @@ import { isMoveValid } from './game-rules.js';
 describe('game-rules', () => {
     describe('isMoveValid', () => {
         describe('invalid', () => {
+            const CreatePiece = (player) => ({
+                player
+            });
+            const CreatePlayer = (name) => ({
+                name
+            });
+            const black = CreatePlayer('black');
+            const white = CreatePlayer('white');
+
             it('if move behind goal', () => {
                 expect(isMoveValid({ index: 16 }))
                     .toEqual(expect.objectContaining({ valid: false }));
             });
-
+            
             it('if out of playfield', () => {
                 expect(isMoveValid({ index: 100 }))
                     .toEqual(expect.objectContaining({ valid: false }));
                 expect(isMoveValid({ index: -100 }))
+                    .toEqual(expect.objectContaining({ valid: false }));
+            });
+
+            it('if space already occupied by own piece', () => {
+                const board = { getPieces: () => [CreatePiece(black)] };
+                expect(isMoveValid({ index: 2, board, player: black }))
+                    .toEqual(expect.objectContaining({ valid: false }));
+                expect(isMoveValid({ index: 6, board, player: black }))
+                    .toEqual(expect.objectContaining({ valid: false }));
+                expect(isMoveValid({ index: 13, board, player: black }))
                     .toEqual(expect.objectContaining({ valid: false }));
             });
         });
