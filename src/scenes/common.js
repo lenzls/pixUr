25a9f8a1abcaf5575ade6ddfe4ac653b8619bfdb
Simon Lenz <post@simon-lenz.de>
2020-05-03 14:25:34 +0200

    lint

--- src/scenes/common.js
+++ src/scenes/common.js
@@ -2,7 +2,7 @@ import { Graphics, Text } from '../engine.js';
 
 export function createButton({ text, color, position, onClick, transparency = 0.8 }) {
     const buttonText = new Text(text);
-    
+
     const background = new Graphics();
     background.beginFill(color, transparency);
     background.drawRect(0, 0, buttonText.width + 20, buttonText.height + 10);
@@ -11,13 +11,13 @@ export function createButton({ text, color, position, onClick, transparency = 0.
     background.buttonMode = true;
     background.on('pointerdown', onClick);
 
-    
+
     buttonText.position.set(
-        background.width / 2 - buttonText.width / 2, 
-        background.height / 2 - buttonText.height / 2, 
+        background.width / 2 - buttonText.width / 2,
+        background.height / 2 - buttonText.height / 2,
     );
     background.addChild(buttonText);
     background.position.set(position.x, position.y);
-    
+
     return background;
 }
