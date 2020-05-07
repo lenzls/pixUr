cc74f907882d5c9ceb519df67d8cf3069f59423b
Simon Lenz <post@simon-lenz.de>
2020-05-07 23:24:49 +0200

    refactor human actor

--- src/player/human-actor.js
+++ src/player/human-actor.js
@@ -2,29 +2,45 @@ export function CreateHumanActor({ me }) {
     let diceRollCallback = null;
     let moveCallback = null;
     let touchPieceCallback = null;
+
+    function useMoveCallback(args) {
+        const toCall = moveCallback;
+        moveCallback = null;
+        toCall(args);
+    }
+    function useDiceCallback(args) {
+        const toCall = diceRollCallback;
+        diceRollCallback = null;
+        toCall(args);
+    }
+    function makePiecesInteractive(action) {
+        touchPieceCallback = action;
+        me.pieces.forEach(({ sprite }) => {
+            sprite.on('pointerdown', touchPieceCallback);
+            sprite.interactive = true;
+        });
+    }
+    function removePiecesInteractiveness() {
+        me.pieces.forEach(({ sprite }) => {
+            sprite.off('pointerdown', touchPieceCallback);
+            sprite.interactive = false;
+        });
+    }
+
     return {
         askWhenToRollDice({ nowCallback }) {
             diceRollCallback = nowCallback;
         },
         askToThinkOfMove({ doneCallback }) {
-            touchPieceCallback = (event) => this.touchPiece({ piece: event.target.piece });
-            me.pieces.forEach(({ sprite }) => {
-                sprite.on('pointerdown', touchPieceCallback);
-                sprite.interactive = true;
-            });
+            makePiecesInteractive((event) => this.touchPiece({ piece: event.target.piece }));
             moveCallback = doneCallback;
         },
         touchDice() {
-            diceRollCallback();
-            diceRollCallback = null;
+            useDiceCallback();
         },
         touchPiece({ piece }) {
-            moveCallback({ piece });
-            moveCallback = null;
-            me.pieces.forEach(({ sprite }) => {
-                sprite.off('pointerdown', touchPieceCallback);
-                sprite.interactive = false;
-            });
+            removePiecesInteractiveness();
+            useMoveCallback({ piece });
         },
     };
 }
