e67b6df1128f25565a916a687b4707c7f16d5e3e
Simon Lenz <post@simon-lenz.de>
2020-05-09 21:12:03 +0200

    rename player type  to actor type

--- src/player/player.js
+++ src/player/player.js
@@ -9,24 +9,24 @@ export const COLOUR = {
     WHITE: 'white',
 };
 
-export const TYPE = {
+export const ACTOR_TYPE = {
     AI: 'ai',
     HUMAN: 'human',
 };
 
-function CreatePlayer({ name, asset, type, colour }) {
+function CreatePlayer({ name, asset, actorType, colour }) {
     const pieces = [];
     const player = {
         name,
         pieces,
-        type,
+        actorType,
         colour,
         actor: null,
     };
-    if (type === TYPE.AI) {
+    if (actorType === ACTOR_TYPE.AI) {
         player.actor = CreateAiActor({ me: player });
     }
-    if (type === TYPE.HUMAN) {
+    if (actorType === ACTOR_TYPE.HUMAN) {
         player.actor = CreateHumanActor({ me: player });
     }
     pieces.push(CreatePiece({ player, asset }));
@@ -44,7 +44,7 @@ export function CreateBlackPlayer() {
         name: 'black',
         asset: ASSETS.BLACK_PIECE,
         colour: COLOUR.BLACK,
-        type: TYPE.HUMAN,
+        actorType: ACTOR_TYPE.HUMAN,
     });
 }
 
@@ -53,6 +53,6 @@ export function CreateWhitePlayer() {
         name: 'white',
         asset: ASSETS.WHITE_PIECE,
         colour: COLOUR.WHITE,
-        type: TYPE.AI,
+        actorType: ACTOR_TYPE.AI,
     });
 }
